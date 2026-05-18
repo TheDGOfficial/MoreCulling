fd59ebb63aa67ba22f59ca73c50ed130e5d11c27
Mustafa <mustafaoncel92@gmail.com>
2026-05-18 13:15:38 +0300

    Refactor shouldDrawSide method with Overwrite

--- common/src/main/java/ca/fxco/moreculling/mixin/compat/BlockOcclusionCache_sodiumMixin.java
+++ common/src/main/java/ca/fxco/moreculling/mixin/compat/BlockOcclusionCache_sodiumMixin.java
@@ -14,6 +14,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -26,7 +28,6 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Pseudo
 @Mixin(AbstractBlockRenderContext.class)
 public class BlockOcclusionCache_sodiumMixin {
-
     @Shadow
     protected BlockState state;
 
@@ -36,20 +37,14 @@ public class BlockOcclusionCache_sodiumMixin {
     @Shadow
     protected BlockPos pos;
 
-    @Inject(
-            method = "shouldDrawSide",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/renderer/block/BlockAndTintGetter;" +
-                            "getBlockState(Lnet/minecraft/core/BlockPos;" +
-                            ")Lnet/minecraft/world/level/block/state/BlockState;",
-                    shift = At.Shift.BEFORE
-            ),
-            cancellable = true
-    )
-    private void moreculling$useMoreCulling(Direction facing, CallbackInfoReturnable<Boolean> cir, @Local BlockPos.MutableBlockPos adjPos) {
-        if (MoreCulling.CONFIG.useBlockStateCulling) {
-            cir.setReturnValue(CullingUtils.shouldDrawSideCulling(state, level.getBlockState(adjPos), level, pos, facing, adjPos));
-        }
+    @Shadow
+    @Final
+    private BlockPos.MutableBlockPos cachedPositionObject;
+
+    @Overwrite
+    public final boolean shouldDrawSide(final Direction facing) {
+        final var neighborPos = this.cachedPositionObject;
+        neighborPos.setWithOffset(this.pos, facing);
+        return CullingUtils.shouldDrawSideCulling(state, level.getBlockState(neighborPos), level, pos, facing, neighborPos);
     }
 }
